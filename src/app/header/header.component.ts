84f8599318a56ed4bcd425674c9ef8d6f931af24
gm2870 <alifarzanegan70@gmail.com>
2023-12-04 00:58:57 +0330

    added profile dropdown

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,11 +3,22 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-header',
   templateUrl: 'header.component.html',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatMenuModule],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatExpansionModule,
+    MatDividerModule,
+    MatSlideToggleModule,
+  ],
 })
 export class HeaderComponent {}
