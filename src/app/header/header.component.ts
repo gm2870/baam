14926dc4a01776fe1fe5727e422847ac80d6ab5d
gm2870 <alifarzanegan70@gmail.com>
2023-12-05 23:32:02 +0330

    added multi theme options

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,12 +6,15 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: 'header.component.html',
   standalone: true,
   imports: [
+    CommonModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
@@ -19,6 +22,33 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
     MatExpansionModule,
     MatDividerModule,
     MatSlideToggleModule,
+    MatListModule,
   ],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  themes = [
+    {
+      name: 'DEFAULT',
+      label: 'پیش فرض',
+      avatar:
+        'background: linear-gradient(90deg, rgb(248, 155, 23) 50%, rgb(44, 146, 242) 50%);',
+    },
+    {
+      name: 'SPRING',
+      label: 'بهاری',
+      avatar:
+        'background: linear-gradient(90deg, rgb(51, 146, 214) 50%, rgb(234, 54, 108) 50%);',
+    },
+    {
+      name: 'AUTUMN',
+      label: 'پاییزی',
+      avatar:
+        'background: linear-gradient(90deg, rgb(83, 137, 156) 50%, rgb(248, 95, 49) 50%);',
+    },
+  ];
+
+  languages = [
+    { locale: 'fa', label: 'فارسی' },
+    { locale: 'en', label: 'انگلیسی' },
+  ];
+}
