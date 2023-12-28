97d964cf1fe6d39b1cff42324de9c7f10e89a3b5
gm2870 <alifarzanegan70@gmail.com>
2023-12-28 13:24:50 +0330

    added env variables

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,14 +3,14 @@ import { Injectable } from '@angular/core';
 import { Observable, delay } from 'rxjs';
 import { ApiResponse } from '../shared/models/api.model';
 import { LoginCredentials } from './auth.model';
-
+import { environment } from '../../environments/environment';
 @Injectable()
 export class AuthService {
   constructor(private http: HttpClient) {}
 
   login(data: LoginCredentials): Observable<ApiResponse> {
     return this.http
-      .post<ApiResponse>('http://localhost:8080/api/auth/login', data)
+      .post<ApiResponse>(`${environment.baseUrl}/api/auth/login`, data)
       .pipe(delay(1000));
   }
 }
